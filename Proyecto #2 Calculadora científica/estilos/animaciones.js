3ded9dbc1c44206f61bbbda2ae4d0649b7a30905
Luis Covis <luisjose.0j0@gmail.com>
2022-01-16 16:01:29 -0400

    Corregido un error referente al degradado

--- Proyecto #2 Calculadora científica/estilos/animaciones.js
+++ Proyecto #2 Calculadora científica/estilos/animaciones.js
@@ -1,9 +1,15 @@
 // Actualiza coordenadas XY para el centro del degradado radial
 document.querySelectorAll('button').forEach(item => {
+  let h = item.getBoundingClientRect().height;
+  let w = item.getBoundingClientRect().width;
+  if (item.firstChild.style !== undefined){
+    item.firstChild.style.setProperty("--sizeY",h + 'px')
+    item.firstChild.style.setProperty("--sizeX",w + 'px')
+  }
     item.addEventListener('mousemove', e => {
       let rect = e.target.getBoundingClientRect();
       let x = e.clientX - rect.left;
-      let y = e.clientY - rect.top;
+      let y = e.clientY - rect.top + 1;
       item.style.setProperty('--x', x + 'px');
       item.style.setProperty('--y', y + 'px');
     })
